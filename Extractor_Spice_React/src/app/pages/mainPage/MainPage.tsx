8acbac3040831ab8f554534fe24f86f8315bba9b
Alexander Cheprasov <cheprasov_sashuta@mail.ru>
2025-04-19 01:08:34 +0300

    experimental web API to MainPage added

--- Extractor_Spice_React/src/app/pages/mainPage/MainPage.tsx
+++ Extractor_Spice_React/src/app/pages/mainPage/MainPage.tsx
@@ -1,80 +1,87 @@
-import { Button } from "../../../core/UI/Buttons/Button"
-import styles from "./MainPage.module.css"
-import { Graph } from "./widgets/Graph/Graph"
-import { settingsPropps } from "./widgets/Settings/api"
-import { Settings } from "./widgets/Settings/Settings"
+import { useQuery } from "@tanstack/react-query";
+import { Button } from "../../../core/UI/Buttons/Button";
+import styles from "./MainPage.module.css";
+import { Graph } from "./widgets/Graph/Graph";
+import { Settings } from "./widgets/Settings/Settings";
+import chartSettingsGET from "./getSettings";
+import {chartSettingsDataSchema,settingsPropps,} from "./widgets/Settings/api";
 
-export const MainPage = ()=>{
-    const data:settingsPropps["config"]["fieldsets"] = {
-        "chip-number-fieldset": [
-            {
-                "value": "Kristal_0p6_waf0chip1",
-                "checked": "true"
-            }
-        ],
-        "inner-nominal-fieldset": [
-            {
-                "value": "D06p_W35_L3p5"
-            },
-            {
-                "value": "D09p_W100_L1p7",
-                "checked": "true"
-            },
-            {
-                "value": "D16n_W35_L3p5",
-                "checked":"true"
-            },
-            {
-                "value": "D19n_W100_L1p7"
-            }
-        ],
-        "electric-fieldset": [
-            {
-                "value": "soi_dc_idvd"
-            },
-            {
-                "value": "soi_dc_idvg"
-            }
-        ],
-        "temperature-fieldset": [
-            {
-                "value": "300K",
-                "checked": "true"
-            }
-        ]
-    }
-    const settingsConfig:settingsPropps={
-        config:{
-            fieldsets:data,
-            btnAcceptAll:{
-                children: "Выбрать все",
-                type:"button"
-            },
-            btnRejectAll:{
-                children: "сбросили все",
-                type:"button"
-            }
-        },
-        syncFunc:()=>console.log("Синхронизация")
-    }
-    // const queryData = useQuery({queryKey:["r"], queryFn:()=>chartSettingsGET()})
-    // if(queryData.data instanceof Error || !queryData.data?.isSuccessful){
-    //     return(
-    //         <>Упс</>
-    //     )
+export const MainPage = () => {
+  const { data, status } = useQuery({
+    queryKey: ["chart-settings"],
+    queryFn: () => chartSettingsGET(),
+  });
+
+  if (status === "error") return <div>Ошибка</div>;
+  if (status === "pending") return <div>Загрузка...</div>;
+
+  const parsedData = chartSettingsDataSchema.safeParse(data?.data);
+  
+  if (!parsedData.success) {
+    console.error("Validation error:", parsedData.error);
+    return <div>Ошибочка вышла</div>;
+  }
+
+  const settingsConfig: settingsPropps = {
+    config: {
+      fieldsets: parsedData.data,
+      btnAcceptAll: {
+        children: "Выбрать все",
+        type: "button",
+      },
+      btnRejectAll: {
+        children: "Сбросить все",
+        type: "button",
+      },
+    },
+    syncFunc: () => console.log("Синхронизация"),
+  };
+
+  return (
+    <main className={styles.main} id="app">
+      <Graph />
+      <div>
+        <Button type="button">Отправить данные</Button>
+      </div>
+      <Settings outerStyles={styles.settings} {...settingsConfig} />
+    </main>
+  );
+};
+    // const data:settingsPropps["config"]["fieldsets"] = {
+    //     "chip-number-fieldset": [
+    //         {
+    //             value: "Kristal_0p6_waf0chip1",
+    //             checked: "true"
+    //         }
+    //     ],
+    //     "inner-nominal-fieldset": [
+    //         {
+    //             "value": "D06p_W35_L3p5"
+    //         },
+    //         {
+    //             "value": "D09p_W100_L1p7",
+    //             "checked": "true"
+    //         },
+    //         {
+    //             "value": "D16n_W35_L3p5",
+    //             "checked":"true"
+    //         },
+    //         {
+    //             "value": "D19n_W100_L1p7"
+    //         }
+    //     ],
+    //     "electric-fieldset": [
+    //         {
+    //             "value": "soi_dc_idvd"
+    //         },
+    //         {
+    //             "value": "soi_dc_idvg"
+    //         }
+    //     ],
+    //     "temperature-fieldset": [
+    //         {
+    //             "value": "300K",
+    //             "checked": "true"
+    //         }
+    //     ]
     // }
-    // console.log(queryData.data)
-    //return(
-    //    {/* // <>
-        // Данные получены
-        // </>*/})
-    return(
-        <main className={styles.main} id="app">
-            <Graph/>
-            <div>
-            <Button>Отправить данные</Button>
-            </div>
-            <Settings outerStyles={styles["settings"]}{...settingsConfig}/>
-        </main>
-    )
-}
--- Extractor_Spice_React/src/app/pages/mainPage/widgets/Settings/api.ts
+++ Extractor_Spice_React/src/app/pages/mainPage/widgets/Settings/api.ts
@@ -5,8 +5,7 @@ const chartSettingsDataFieldSchema = z.object({
     value: z.string(),
     checked:z.optional(z.union([z.literal("true"),z.literal("false")]))
 })
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const chartSettingsDataSchema = z.record(z.string(), z.array(chartSettingsDataFieldSchema))
+export const chartSettingsDataSchema = z.record(z.string(), z.array(chartSettingsDataFieldSchema))
 
 export type settingsPropps = {
     config: {
